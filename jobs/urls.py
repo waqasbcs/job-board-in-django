b2dd75225165c2310ec068a9f6127ce6e8b3397c
waqasbcs <waqasaliy2945@gmail.com>
2024-06-02 10:36:14 +0500

    creating a simple job board using python django

---
+++ jobs/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.job_list, name='job_list'),
+    path('job/<int:job_id>/<slug:slug>/', views.job_detail, name='job_detail'),
+    path('job/<int:job_id>/apply/', views.apply_for_job, name='apply_for_job'),
+]
---
+++ jobs/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Job
+from .forms import ApplicationForm
+
+def job_list(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs/job_list.html', {'jobs': jobs})
+
+def job_detail(request, job_id, slug):
+    job = get_object_or_404(Job, pk=job_id, slug=slug)
+    return render(request, 'jobs/job_detail.html', {'job': job})
+
+def apply_for_job(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST, request.FILES)
+        if form.is_valid():
+            application = form.save(commit=False)
+            application.user = request.user
+            application.job = job
+            application.save()
+            return redirect('job_detail', job_id=job.id)
+    else:
+        form = ApplicationForm()
+    return render(request, 'jobs/apply_for_job.html', {'form': form, 'job': job})
